438f0d59550941981397c07562921f1ca5919511
Ethan Tidmore <ethantidmore06@gmail.com>
2026-03-23 21:15:20 -0500

    setuparch.sh stuff

--- setuparch.sh
+++ setuparch.sh
@@ -2,9 +2,6 @@
 
 # My Arch setup script (base-devel is needed for this)
 
-# NOTE	
-# Please read what this script does. Don't run commands from the internet if you don't know what they're doing!
-
 # Create used directories
 mkdir -p /home/"$USER"/Screenshots /home/"$USER"/.config/{bspwm,gtk-3.0,nvim,polybar,ranger,sxhkd} \
 	/home/"$USER"/.local/{bin,wallpapers} 
@@ -17,14 +14,14 @@ makepkg -si
 cd ..
 
 # Install useful software
-yay -S arp-scan aws-vault bind bitwarden blueberry bspwm dmenu eza feh firejail gdb go gvim hashcat ltrace \
-	lxappearance macchanger maim man-pages mpv nano neovide neovim nmap noto-fonts noto-fonts-cjk \
+yay -S  aerc arp-scan aws-vault bind bitwarden breeze-cursors bspwm dmenu dosfsntools eza feh firejail gdb go gvim  \
+	hashcat ltrace lxappearance macchanger maim man-pages mpv nano neovide neovim nmap noto-fonts noto-fonts-cjk \
 	noto-fonts-emoji noto-fonts-extra picom polybar pulsemixer python-pipx ranger shellcheck strace sxhkd \
 	tcpdump tor torbrowser-launcher tree-sitter-cli ttf-hack-nerd ueberzugpp unclutter unzip valgrind vlc \
 	wget wireshark-qt xclip xorg xorg-xinit yt-dlp zip zsh zsh-syntax-highlighting
 
 # Download software from aur
-yay -S brave-bin gruvbox-dark-gtk htop-vim prismlauncher webcord xcursor-breeze 
+yay -S brave-bin gruvbox-dark-gtk htop-vim prismlauncher webcord
 
 # Install dotfiles
 cp .fehbg /home/"$USER"
@@ -43,18 +40,14 @@ cp sxhkdrc /home/"$USER"/.config/sxhkd
 # Scripts
 cp scripts/* /home/"$USER"/.local/bin
 
-# Install vim plug
-curl -fLo /home/"$USER"/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Download and install zsh-colored-man-pages (read this script before runnig it)
+# Download and install zsh-colored-man-pages
 cd zsh-colored-man-pages || exit
 makepkg -si
 cd ..
 
-# Enable pulsewire-pulse
-systemctl --user enable pipewire-pulse
-systemctl --user start pipewire-pulse 
+# Enable pulsewire
+systemctl --user enable pipewire
+systemctl --user start pipewire
 
 # Lastly setup my st and dmenu build
 cd st-0.8.4 || exit
