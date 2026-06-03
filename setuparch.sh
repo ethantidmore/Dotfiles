04617e2eebe774d82bad3e60f420f430b182ba4f
Ethan Tidmore <ethantidmore06@gmail.com>
2026-06-02 22:28:09 -0500

    add spotify and using vesktop again

--- setuparch.sh
+++ setuparch.sh
@@ -14,16 +14,16 @@ makepkg -si
 cd ..
 
 # Install useful software
-yay -S  aarch64-linux-gnu-gcc aerc arp-scan aws-vault b4 bind bitwarden breeze-cursors bspwm dmenu dosfstools \
+yay -S  aarch64-linux-gnu-gcc aerc arp-scan aws-vault b4 bind bitwarden breeze-cursors bspwm clang dmenu dosfstools \
 	dtc eza feh firejail gdb go gvim hashcat libyaml ltrace lxappearance macchanger maim man-pages mpv nano \
 	neovide neovim nmap noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra perl-authen-sasl \
 	perl-io-socket-ssl picom polybar prismlauncher pulsemixer python-gitpython python-pipx python-ply 
-	ranger swig strace sxhkd tcpdump tor torbrowser-launcher tree-sitter-cli ttf-hack-nerd ueberzugpp \
-	unclutter unzip valgrind vlc wget wireshark-qt xclip xorg xorg-xinit yamllint yt-dlp zip zsh \
-	zsh-syntax-highlighting
+	ranger rust rust-bindgen rust-src swig strace sxhkd tcpdump tor torbrowser-launcher tree-sitter-cli \ 
+	ttf-hack-nerd ueberzugpp unclutter unzip valgrind vlc wget wireshark-qt xclip xorg xorg-xinit yamllint \ 
+	yt-dlp zip zsh zsh-syntax-highlighting
 
 # Download software from aur
-yay -S brave-bin gruvbox-dark-gtk htop-vim webcord
+yay -S brave-bin gruvbox-dark-gtk htop-vim spotify vesktop-bin
 
 # Install python packages
 pipx install dtschema
