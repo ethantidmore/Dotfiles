7a04ce50d62b7b535ba5d6b369127a29bddf64ad
Ethan Tidmore <ethantidmore06@gmail.com>
2026-09-12 21:18:14 -0500

    add xdg thingy

--- setuparch.sh
+++ setuparch.sh
@@ -13,14 +13,14 @@ cd yay || exit
 makepkg -si
 cd ..
 
-# Install useful software
+# Install usefull software
 yay -S  aarch64-linux-gnu-gcc aerc arp-scan b4 bind bitwarden breeze-cursors bspwm clang dmenu dosfstools \
 	dtc eza feh firejail gdb go gvim hashcat libyaml ltrace lxappearance macchanger maim man-pages mpv nano \
 	neovide neovim nmap noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra perl-authen-sasl \
 	perl-io-socket-ssl picom polybar prismlauncher pulsemixer python-gitpython python-pipx python-ply \
 	ranger rust rust-bindgen rust-src spotify-launcher strace swig sxhkd tcpdump tor torbrowser-launcher \
-	tree-sitter-cli ttf-hack-nerd ueberzugpp unclutter unzip valgrind vlc wget wireshark-qt xclip xorg \
-	xorg-xinit yamllint yt-dlp zip zsh zsh-syntax-highlighting
+	tree-sitter-cli ttf-hack-nerd ueberzugpp unclutter unzip valgrind vlc wget wireshark-qt \
+	xdg-desktop-portal-gtk xclip xorg xorg-xinit yamllint yt-dlp zip zsh zsh-syntax-highlighting
 
 # Download software from aur
 yay -S brave-bin gruvbox-dark-gtk htop-vim vesktop-bin
